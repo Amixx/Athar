54b55c0324a929dea00326603d77f63db2e32047
Imants Liepiņš <iliepins01@gmail.com>
2026-03-11 22:37:57 +0100

    Implement canonical serializer for the diff engine.

---
+++ tests/test_canonical_serializer.py
@@ -0,0 +1,17 @@
+from athar.canonical_serializer import build_class_record, build_entity_record, serialize_records
+
+
+def test_serializer_orders_by_id():
+    rec_a = build_entity_record(entity_id="H:2", entity_type="IfcWall", attributes={})
+    rec_b = build_entity_record(entity_id="G:1", entity_type="IfcWall", attributes={})
+    rec_c = build_class_record(
+        class_id="C:9",
+        entity_type="IfcCartesianPoint",
+        old_count=2,
+        new_count=1,
+        exemplar={"Coordinates": {"kind": "list", "items": []}},
+    )
+
+    output = serialize_records([rec_a, rec_b, rec_c]).splitlines()
+    ids = [line.split("\"id\":\"", 1)[1].split("\"", 1)[0] for line in output]
+    assert ids == ["C:9", "G:1", "H:2"]
